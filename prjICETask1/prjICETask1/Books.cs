d6c245a709eef8beb025ab06c0caf27534139925
ST10284732 <ST10284732@vcconnect.edu.za>
2024-07-25 15:13:24 +0200

    Added PROG6212 - ICE Task 1

---
+++ prjICETask1/prjICETask1/Books.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjICETask1
+{
+    public class Books
+    {
+        double bookID;
+        string bookTitle, bookAuthor;
+        double price;
+        public Books(double bookID, string bookTitle, string bookAuthor, double price)
+        {
+            this.bookID = bookID;
+            this.bookTitle = bookTitle;
+            this.bookAuthor = bookAuthor;
+            this.price = price;
+        }
+
+        public object this[int index]
+            {
+                get
+                {
+                    if (index == 0)
+                        return this.bookID;
+
+                    else if (index == 1)
+                        return this.bookTitle;
+
+                    else if (index == 2)
+                        return this.bookAuthor;
+
+                    else if (index == 3)
+                        return this.price;
+                    return null;
+                }
+
+                
+                set
+                {
+                    if (index == 0)
+                        this.bookID = (double)value;
+
+                    else if (index == 1)
+                        this.bookTitle = (string)value;
+
+                    else if (index == 2)
+                        this.bookAuthor = (string)value;
+
+                    else if (index == 3)
+                        this.price = (double)value;
+                }
+            }
+
+        public object this[string attributes]
+        {
+            get
+            {
+                if (attributes.ToLower().Equals("bookID"))
+                    return this.bookID;
+                else if (attributes.ToLower().Equals("bookTitle"))
+                    return this.bookTitle;
+                else if (attributes.ToLower().Equals("bookAuthor"))
+                    return this.bookAuthor;
+                else if (attributes.ToLower().Equals("price"))
+                    return this.price;
+                return null;
+            }
+
+            set
+            {
+                if (attributes.ToLower().Equals("bookID"))
+                    this.bookID = (double)value;
+
+                else if (attributes.ToLower().Equals("bookTitle"))
+                    this.bookTitle = (string)value;
+
+                else if (attributes.ToLower().Equals("bookAuthor"))
+                    this.bookAuthor = (string)value;
+
+                else if (attributes.ToLower().Equals("price"))
+                    this.price = (double)value;
+
+            }
+        }
+
+
+    }
+}
+
+    
+
---
+++ prjICETask1/prjICETask1Tests/BooksTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using prjICETask1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjICETask1.Tests
+{
+    [TestClass()]
+    public class BooksTests
+    {
+        [TestMethod()]
+        public void BooksTest()
+        {
+            Books emp = new Books(1, "Adventures of Amber's love life", "Amber Boucher", 600);
+
+            double expected = 1;
+            double result = Convert.ToDouble(emp[0]);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void BooksTest1()
+        {
+            Books emp = new Books(2, "Kelly's OCD", "Kelly Botha", 200);
+
+            double expected = 2;
+            double result = Convert.ToDouble(emp[0]);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void BooksTest2()
+        {
+            Books emp = new Books(3, "Helga's Confusion", "Helga Mboni", 350);
+
+            double expected = 3;
+            double result = Convert.ToDouble(emp[0]);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void BooksTest3()
+        {
+            Books emp = new Books(4, "George's lost braincells", "George Bekker", 750);
+
+            double expected = 4;
+            double result = Convert.ToDouble(emp[0]);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void BooksTest4()
+        {
+            Books emp = new Books(5, "The IDK Chronicles", "I D. Know", 275);
+
+            double expected = 5;
+            double result = Convert.ToDouble(emp[0]);
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+}
